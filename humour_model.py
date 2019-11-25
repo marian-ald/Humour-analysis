2f006cbb8c8a1f4de228d8659e657ce814995eeb
Marian Aldescu <marian.aldescu11@gmail.com>
2019-11-25 18:59:22 +0200

    Removed unused imports

--- humour_model.py
+++ humour_model.py
@@ -7,15 +7,7 @@ import numpy as np
 import tensorflow as tf
 
 #############
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Dropout
 from keras.layers.core import Dense
-from keras.layers import Flatten
-from keras.layers import Input
 from keras.models import Model
 from keras.models import load_model
 import csv
